e4a098506bc12e40c34e15efd819403aa33085cd
Rob Williams <robwill815@gmail.com>
2023-09-14 05:31:33 -0500

    adding PowerShell script

---
+++ prlock_killer.ps1
@@ -0,0 +1,7 @@
+﻿# Get all .prlock files in the current directory and all subdirectories
+$prlockFiles = Get-ChildItem -Path "." -Filter "*.prlock" -Recurse
+
+# Delete all .prlock files
+foreach ($prlockFile in $prlockFiles) {
+    Remove-Item -Path $prlockFile -Force
+}
